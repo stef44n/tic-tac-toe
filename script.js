81279c8a8d061863766cb2235d4410b8ea93fc54
Stefan <stefanseifeddine@gmail.com>
2022-08-24 03:31:57 +0100

    Add display module

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const game = (() => {
     let x = 'x';
-    let o = 'o';
-    let row1 = [x, o, x];
-    let row2 = [x, o, x];
-    let row3 = [o, x, o];
-    const gameboard = [row1, row2, row3];
+    let o = 'oo';
+    // let row1 = [1, 2, 3];
+    // let row2 = [4, 5, 6];
+    // let row3 = [7, 8, 9];
+    // const gameboard = [row1, row2, row3];
+    const gameboard = [1, 2, 3, '', 5, '', 7, 8, 9];
     
     return {
         gameboard,
-        row1,
-        row2,
-        row3,
         o,
         x,
     };
@@ -22,4 +20,27 @@ const Person = (name) => {
     return {sayName};
 };
 
-// game.gameboard[0][0] = 4;
+// game.gameboard[0] = 444;
+
+const displayController = (() => {
+    const container = document.getElementById('gameboard');
+    for (let i = 0; i < game.gameboard.length; i++) {
+        if (game.gameboard[i] != ' ') {
+            let square = document.createElement('div');
+            square.className = 'cell';
+            square.setAttribute('value', i + 1);
+            square.innerText = game.gameboard[i];
+            container.append(square);
+        };
+    };
+    
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('click', () => {
+            console.log(cell.textContent);
+            cell.innerText = game.o;
+        });
+    });
+
+    // return {cells}
+})();
