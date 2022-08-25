7ed6866364b6c4526a4d3c4f549f1fba2b2f758d
Stefan <stefanseifeddine@gmail.com>
2022-08-25 23:33:17 +0100

    Add Turn toggle & disable pickingUnavailableFields

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const game = (() => {
     // let row2 = [4, 5, 6];
     // let row3 = [7, 8, 9];
     // const gameboard = [row1, row2, row3];
-    const gameboard = [1, 2, 3, ' ', 5, ' ', 7, 8, 9];
+    const gameboard = ['','','','','','','','',''];
     
     return {
         gameboard,
@@ -16,6 +16,8 @@ const game = (() => {
 
 const Person = (name) => {
     const sayName = () => console.log(`my name is ${name}`);
+    // p1 = x
+    // p2 = o
 
     return {sayName};
 };
@@ -25,7 +27,7 @@ const Person = (name) => {
 const displayController = (() => {
     const container = document.getElementById('gameboard');
     for (let i = 0; i < game.gameboard.length; i++) {
-        if (game.gameboard[i] != '') { //1 true
+        if (true) { //1 true
             let square = document.createElement('div');
             square.className = 'cell';
             // square.setAttribute('value', i + 1);
@@ -35,7 +37,11 @@ const displayController = (() => {
         };
     };
     
-    const cells = document.querySelectorAll('.cell');
+    const cells = document.querySelectorAll('div.cell');
+
+    const player1 = 'x';
+    const player2 = 'o';
+    let plTurn = player1;
     // cells.forEach((cell) => {
     //     cell.addEventListener('click', () => {
     //         console.log(cell.textContent);
@@ -47,11 +53,27 @@ const displayController = (() => {
     cells.forEach((cell) => {
         // console.log(cell.id);
         cell.onclick = function() {
-            cell.style.backgroundColor = 'yellow';
-            console.log(cell.id);
-            game.gameboard[cell.id - 1] = Number(cell.id) + 10 + 't'; // X or O
-            console.log(game.gameboard);
-            cell.innerText = game.gameboard[cell.id - 1];
+            if (game.gameboard[cell.id - 1] == '') {
+                // cell.style.backgroundColor = 'yellow';
+                cell.className += ' hover';
+                console.log(cell.id);
+                game.gameboard[cell.id - 1] = plTurn; // X or O
+                console.log(game.gameboard);
+                cell.innerText = game.gameboard[cell.id - 1];
+
+                if (plTurn == player1) {
+                    // cell.innerText = 'x'
+                    cell.style.backgroundColor = 'yellow'
+                    // console.log(plTurn)
+                    plTurn = player2;
+                    console.log(plTurn)
+                } else {
+                    // cell.innerText = 'o'
+                    cell.style.backgroundColor = 'skyblue'
+                    plTurn = player1;
+                    console.log(plTurn)
+                };
+            };
         }; 
     });
 
